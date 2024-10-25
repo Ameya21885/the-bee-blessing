a951a0da23651841a2566222b65c64c840d7120e
Ameya Chandrashekhar Thakur <ameyathakur21@gmail.com>
2024-10-25 12:13:36 +0530

    admin-navbar

---
+++ src/components/admin/ANavbar.jsx
@@ -0,0 +1,102 @@
+"use client";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import {
+    AppBar,
+    Box,
+    Button,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+import { useState } from "react";
+
+const ANavbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={() => toggleDrawer(false)}
+      onKeyDown={() => toggleDrawer(false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#202020" }}>
+        <Toolbar variant="dense">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor="left"
+            open={open}
+            onClose={() => toggleDrawer(false)}
+            PaperProps={{
+              sx: {
+                backgroundColor: "#fafafa", // Drawer background color
+                width: 250, // Drawer width
+                boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)", // Custom shadow
+              },
+            }}
+          >
+            {DrawerList}
+          </Drawer>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            The-Bee-Blessing's
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default ANavbar;
